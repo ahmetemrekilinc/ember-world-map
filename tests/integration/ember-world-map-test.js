bc736bf6968e025112355e03fbb1165616df4df8
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-02 01:22:58 -0400

    Ran codemods to remove jQuery

--- tests/integration/ember-world-map-test.js
+++ tests/integration/ember-world-map-test.js
@@ -1,11 +1,13 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('ember-world-map', 'Integration | Component | ember-world-map', {
-  integration: true
-});
+module('Integration | Component | ember-world-map', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  this.render(hbs`{{ember-world-map}}`);
-  assert.equal(this.$(".ember-world-map").length, 1);
+  test('it renders', async function(assert) {
+    await render(hbs`{{ember-world-map}}`);
+    assert.equal(findAll(".ember-world-map").length, 1);
+  });
 });
